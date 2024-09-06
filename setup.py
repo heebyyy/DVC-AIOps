523024fdbdd8ba06b6f4b0eff29b2cf96455e6da
Bolaji Toheeb <olayemibolaji1@gmail.com>
2024-09-06 22:16:02 +0100

    basic structure

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Bolaji",
+    description="A small package for dvc ml pipeline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/heebyyy/DVC-AIOps",
+    author_email="olayemibolaji1@gmail.com",
+    packages=["src"],
+    python_requires=">=3.12",
+    install_requires=[
+        'dvc',
+        'tensorflow',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'tqdm',
+        'pyYAML',
+        'boto3'
+    ]
+)
